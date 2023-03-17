92c23b2a654dfcfc35f9c67561b10bf6327ee66b
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-03-16 23:20:44 -0700

    update

--- list_objects.py
+++ list_objects.py
@@ -1,7 +1,6 @@
 import os
 import json
 import datetime
-
 now = datetime.datetime.now()
 [year, month, day, hour, minute, second] = [str(now.year).zfill(4),
                                             str(now.month).zfill(2),
@@ -9,14 +8,16 @@ now = datetime.datetime.now()
                                             str(now.hour).zfill(2),
                                             str(now.minute).zfill(2),
                                             str(now.second).zfill(2)]
+ts = ''.join([year, month, day, hour, minute, second])  # time stamp
 
-ts = ''.join([year, month, day, hour, minute, second])
-
-data = os.popen('aws s3api list-objects --no-sign-request --bucket sentinel-products-ca-mirror').read()
-
+data = os.popen(' '.join(['aws',  # read data from aws
+                          's3api',
+                          'list-objects',
+                          '--no-sign-request',
+                          '--bucket sentinel-products-ca-mirror'])).read()
 df = ts + '_objects.txt'  # file to write
 print('+w', df)
 open(df, 'wb').write(data.encode())  # write json data to file
 
-d = json.loads(data)
+d = json.loads(data)  # load the json-format data
 print(d)
