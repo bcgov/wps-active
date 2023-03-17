1d2bf084c0378961727da5e481c5007f6cd90c06
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-03-16 23:31:46 -0700

    update

--- list_objects.py
+++ list_objects.py
@@ -1,7 +1,11 @@
 import os
 import json
 import datetime
-now = datetime.datetime.now()
+
+# row ID over BC from our records (thanks adyk)
+bc_row = os.popen("python3 ~/GitHub/wps-research/py/sentinel2_bc_tiles_shp/bc_row.py").read().strip().split()
+
+now = datetime.datetime.now()  # create timestamp
 [year, month, day, hour, minute, second] = [str(now.year).zfill(4),
                                             str(now.month).zfill(2),
                                             str(now.day).zfill(2),
@@ -15,11 +19,26 @@ data = os.popen(' '.join(['aws',  # read data from aws
                           'list-objects',
                           '--no-sign-request',
                           '--bucket sentinel-products-ca-mirror'])).read()
+
 df = ts + '_objects.txt'  # file to write
 print('+w', df)
 open(df, 'wb').write(data.encode())  # write json data to file
 
 d = json.loads(data)  # load the json-format data
-data = d['Contents']  # extract the data records, one per dataset
+data, rows = d['Contents'], []  # extract the data records, one per dataset
 
+for d in data:
+    key = d['Key'].strip()
+    w = [x.strip() for x in key.split('/')]
+    if w[0] == 'Sentinel-2':
+        f = w[-1]
+        row = f.split('_')[5][1:]
+        rows += [row]
 
+rows = list(set(rows))
+print(rows)
+print(len(rows))
+
+bc_row = os.popen("python3 ~/GitHub/wps-research/py/sentinel2_bc_tiles_shp/bc_row.py").read().strip().split()
+# print(bc_row)
+print(len(bc_row))
