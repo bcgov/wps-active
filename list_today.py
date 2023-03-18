82b815ddeeb83d818e45ff4d6526f29f13ef328b
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-03-17 18:39:27 -0700

    update

--- list_today.py
+++ list_today.py
@@ -4,7 +4,7 @@ import sys
 import datetime
 
 bc_row = os.popen("python3 ~/GitHub/wps-research/py/sentinel2_bc_tiles_shp/bc_row.py").read().strip().split()
-print(bc_row)
+print("bc row-id under obs:", bc_row)
 
 if len(os.popen("aws 2>&1").read().split("not found")) > 1:
     print('Need to install aws cli:')
@@ -44,20 +44,31 @@ c2_d = get(c2 + cd) # Level-2 data listings
 open('S2MSI2A_' + cd.replace('/', ''), 'wb').write(c2_d.encode())
 
 
-lines = [x.split() for x in c2_d.strip().split('\n')]
-for line in lines:
-    print(line)
+def my_list(c2_d):
+    lines, row_id = None, None
+    try:
+        lines = [x.split() for x in c2_d.strip().split('\n')]
 
-row_id = [line[-1].split('_')[5][1:] for line in lines]
-print(len(row_id))
-row_id = list(set(row_id))
-print(len(row_id))
+        for line in lines:
+            print(line)
 
-print(row_id)
-print(bc_row)
+        row_id = [line[-1].split('_')[5][1:] for line in lines]
+        row_id = list(set(row_id))
+    except:
+        return
 
-for r in row_id:
-    if r not in bc_row:
-        print("Warning: ", r)
+    print("row id observed today:", len(row_id))
 
+    print("row ids observed today:", row_id)
+    # print(bc_row)
+
+    for r in row_id:
+        if r not in bc_row:
+            pass # print("Warning: ", r)
+
+
+print("LEVEL 1:")
+my_list(c1_d)
+print("LEVEL 2:")
+my_list(c2_d)
 # aws s3 cp  --no-sign-request s3://sentinel-products-ca-mirror/Sentinel-2/S2MSI1C/2023/03/08/S2B_MSIL1C_20230308T190239_N0509_R013_T10UFB_20230308T223026.zip S2B_MSIL1C_20230308T190239_N0509_R013_T10UFB_20230308T223026.zip
