1206f3076bfa126b85345fc65156d8cc61cddbb8
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-06-03 19:52:15 -0700

    update

---
+++ py/CWFIS_active_fires.py
@@ -0,0 +1,28 @@
+'''20230603 query CIFFC active fire locations
+
+'''
+from urllib.request import urlopen
+import sys
+import json
+
+url = 'https://cwfis.cfs.nrcan.gc.ca/geoserver/public/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=public:activefires_current&outputFormat=json'
+response = urlopen(url)
+data_json = json.loads(response.read())
+# print(data_json)
+
+select_agency = None
+if len(sys.argv) > 1:
+    select_agency = sys.argv[1]
+
+agency = {}
+for f in data_json['features']:
+    # print(f.keys()) # dict_keys(['type', 'id', 'geometry', 'geometry_name', 'properties'])
+    properties = f['properties']
+    ag = properties['agency']
+    if ag not in agency:
+        agency[ag] = 0
+    agency[ag] += 1
+    if ag == select_agency or select_agency is None:
+        print(properties)
+
+print(agency)
