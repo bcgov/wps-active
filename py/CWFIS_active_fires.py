9ed1068e8e95d9eb21b85f6c8e23e3f92b7af116
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-06-06 17:10:21 -0700

    update

--- py/CWFIS_active_fires.py
+++ py/CWFIS_active_fires.py
@@ -7,21 +7,23 @@ import shutil
 import json
 import sys
 
-# read json data
-url = 'https://cwfis.cfs.nrcan.gc.ca/geoserver/public/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=public:activefires_current&outputFormat=json'
-response = urlopen(url)
-data_json = response.read()
+data_json = None  # read json data
+try:  
+    url = 'https://cwfis.cfs.nrcan.gc.ca/geoserver/public/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=public:activefires_current&outputFormat=json'
+    response = urlopen(url)
+    data_json = response.read()
 
-# save to file
-ts = time_stamp()
-jfn = ts + '_CWFIS_fires.json'
-open(jfn, 'wb').write(data_json)
-print('+w', jfn)
+    # save to file
+    ts = time_stamp()
+    jfn = ts + '_CWFIS_fires.json'
+    open(jfn, 'wb').write(data_json)
+    print('+w', jfn)
 
-# convert to shapefile
-shutil.copyfile(jfn, 'CWFIS_fires.json')
-run('ogr2ogr -skipfailures -f "ESRI Shapefile" CWFIS.shp CWFIS_fires.json')
-
+    # convert to shapefile
+    shutil.copyfile(jfn, 'CWFIS_fires.json')
+    run('ogr2ogr -skipfailures -f "ESRI Shapefile" CWFIS.shp CWFIS_fires.json')
+except:
+    data_json = open('CWFIS_fires.json').read()
 
 # parse json data
 data_json = json.loads(data_json)
@@ -33,7 +35,7 @@ if len(sys.argv) > 1:
 
 agency = {}
 for f in data_json['features']:
-    # print(f.keys()) # dict_keys(['type', 'id', 'geometry', 'geometry_name', 'properties'])
+    print(f.keys()) # dict_keys(['type', 'id', 'geometry', 'geometry_name', 'properties'])
     properties = f['properties']
     ag = properties['agency']
     if ag not in agency:
