0848e92b2f8215adaa6beeff9002953da3ce655d
Ashlin Richardson <ashlin.richardson@gmail.com>
2024-06-21 22:09:03 -0700

    update

--- py/update_listing.py
+++ py/update_listing.py
@@ -1,5 +1,9 @@
-from misc import timestamp, exists
+'''20240621 update listing of objects in nrcan sentinel products mirror
+'''
+from misc import timestamp, exists, sep
 import os
+
+my_path = sep.join(os.path.abspath(__file__).split(sep)[:-1]) + sep
 
 def update_listing():
     ts = timestamp()
@@ -17,5 +21,14 @@ def update_listing():
     df = my_path + 'listing' + sep + ts + '_objects.txt'  # file to write
     open(df, 'wb').write(data.encode())  # record json to file
 
+def latest_listing():
+    listings = os.popen('ls -1 '  + my_path + 'listing' + sep + '*_objects.txt').readlines()
+    listings = [[x.split(sep)[-1].split('_')[0], x.strip()] for x in listings]
+    listings.sort()  # sort on increasing time
+    latest = listings[-1][1]  #  most recent entry
+    print('+r', latest)
+    return open(latest).read()
+
 if __name__ == '__main__':
     update_listing()
+    d = latest_listing()
